6099446c3468e473df74176d10c872f608a9ba18
mike <mikethecodegeek@gmail.com>
2020-12-30 15:16:49 -0800

    add tabs to search page

--- auth-front-end/src/components/SearchResults/index.js
+++ auth-front-end/src/components/SearchResults/index.js
@@ -1,51 +1,68 @@
-import {useSelector,useDispatch} from 'react-redux';
-import {useEffect} from 'react'
-import {useParams,useHistory} from 'react-router-dom'
-import {searchBeersAndBrews} from '../../store/search'
-
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { searchBeersAndBrews } from "../../store/search";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import "./searchResults.css"
 
 const SearchResults = () => {
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const { name } = useParams();
-    const beer = useSelector(state => state.search);
-   
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const { name } = useParams();
+  const beer = useSelector((state) => state.search);
 
-    useEffect(()=>{
-        dispatch(searchBeersAndBrews(name))
-    },[name])
+  useEffect(() => {
+    dispatch(searchBeersAndBrews(name));
+  }, [name]);
 
-    const handleBreweryClick = (id) => {
-        history.push(`/breweries/${id}`)
-    }
+  const handleBreweryClick = (id) => {
+    history.push(`/breweries/${id}`);
+  };
 
-    const handleBeerClick = (id) => {
-        history.push(`/beers/${id}`)
-    }
+  const handleBeerClick = (id) => {
+    history.push(`/beers/${id}`);
+  };
 
-    return (
-        <div>
-            <h2>Search</h2>
-            <h3>Beers</h3>
-            {
-              beer.beers ? beer.beers.map(beer=> {
-              return <div>
-                 <p onClick={()=>handleBeerClick(beer.id)}>{beer.name}</p>
-                 <p onClick={()=>handleBreweryClick(beer.Brewery.id)}>{beer.Brewery.name}</p>
-                 <p>{beer.Type.name}</p>
+  return (
+    <div className='container'>
+      <Tabs>
+        <TabList>
+          <Tab>Beers {beer.beers && beer.beers.length}</Tab>
+          <Tab>Breweries {beer.brewers && beer.brewers.length}</Tab>
+        </TabList>
+
+        <TabPanel>
+        {beer.beers
+        ? beer.beers.map((beer) => {
+            return (
+              <div className="section">
+                <p className="link" onClick={() => handleBeerClick(beer.id)}>{beer.name}</p>
+                <p className='link beerSearchBrew' onClick={() => handleBreweryClick(beer.Brewery.id)}>
+                  {beer.Brewery.name}
+                </p>
+                <p className="gray beerSearchType">{beer.Type.name}</p>
               </div>
-            }) : []
-            }
-            <h3>Breweries</h3>
-            { 
-             beer.brewers ? beer.brewers.map(brewer=> <p onClick={()=>handleBreweryClick(brewer.id)}>{brewer.name}</p>) : []
-            }
-            {
-            //    <p>{beer.name}</p>
-            }
+            );
+          })
+        : []}
+        </TabPanel>
+        <TabPanel>
+        {beer.brewers
+        ? beer.brewers.map((brewer) => (
+            <p onClick={() => handleBreweryClick(brewer.id)}>{brewer.name}</p>
+          ))
+        : []}
+      {
+        //    <p>{beer.name}</p>
+      }
+        </TabPanel>
+      </Tabs>
 
-        </div>
-    )
-}
+     
+    
+    </div>
+  );
+};
 
 export default SearchResults;
